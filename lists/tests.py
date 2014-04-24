30152c7d1a5a5d5e5b9ff66d26077fedfc8bab7f
Ben Morris <ben_morris@zyen.com>
2014-04-24 19:07:13 +0100

    new URL view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,23 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
 
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/') 
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -37,17 +54,7 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
 
-        self.assertEqual(response.content.strip().decode(), expected_html)
-        
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertEqual(response.content.strip().decode(), expected_html)       
         
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -68,7 +75,7 @@ class HomePageTest(TestCase):
         response = home_page(request)
         
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
